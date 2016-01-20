fc732ae92b695c9eeb28ee1e8bb7be7f102e6f60
Core i7 <Core i7@192.168.1.105>
2016-01-20 10:11:43 -0500

    "Estoy agregando un trabajo de expocicion"

---
+++ Pandita-Ciclo2/src/pe/edu/upeu/poo/Polimorfismo/Polimorfismo.java
@@ -0,0 +1,22 @@
+package pe.edu.upeu.poo.Polimorfismo;
+
+import java.util.Scanner;
+
+public class Polimorfismo {
+
+	public static void main(String[] args) {
+		 Scanner hoa = new Scanner(System.in);
+	        Saludo x=new Saludo();
+	        System.out.println("Ingrese su nombre: ");
+	        String nom1 = hoa.nextLine();
+	        System.out.println("Ingrese su nombre: ");
+	        String nom2 = hoa.nextLine();
+	        Saludo y=new Saludo("Hola "+ nom1);
+	        Saludo z=new Saludo("Hola", nom2);
+	        System.out.println(x.MensajeSaludo);
+	        System.out.println(y.MensajeSaludo);
+	        System.out.println(z.MensajeSaludo);
+
+	}
+
+}
